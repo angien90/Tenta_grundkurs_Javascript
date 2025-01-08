24e9b1e02cbbea3fdf2e7650ce6f632532f0480b
angien90 <angelicanylander@hotmail.com>
2025-01-08 10:12:13 +0100

    Add: Pseudokod för lejonmataren & js-fil för hjärtespelet

--- lejonmataren.js
+++ lejonmataren.js
@@ -1,3 +1,23 @@
+/** 
+* Deklarera variabel antalMåltiderIdag som 0
+* Deklarera variabel lampaPå som falskt
+* Skapa funktion för mataLejonet:
+    Om antalMåltiderIdag är mindre än 5:
+        Visa ett meddelande "Nom nom nom! Lejonet är mätt."
+        Öka antalMåltiderIdag med 1
+    Annars:
+        Visa ett meddelande "Lejonet har ätit för mycket idag!"
+* Skapa funktion tändSläckLampa:
+    Växla värdet på lampaPå (om den är sann, gör den falsk; om den är falsk, gör den sann)
+    Om lampaPå är sann:
+        Visa meddelandet "Lampan är tänd"
+    Annars:
+        Visa meddelandet "Lampan är släckt"
+* Skapa funktion tömToalett:
+    Skriv "swoosh" i konsolen
+    Visa meddelandet "Toaletten är tömd!"
+*/
+
 let antalMåltiderIdag = 0;
         let lampaPå = false;
 
